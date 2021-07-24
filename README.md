dc754d3842e92d2fd3964f356739397f891ac5d1
Greg Wuller <greg@afofo.com>
2021-07-23 21:45:59 -0700

    initial example

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # norns-example-mod
-example mod for matron
+
+this repo demonstrates how to create a "mod" which can augment/patch/hack matron
+(the lua scripting engine for norns).
+
+## why
+
+a mod lives in the `dust` folder just like normal norns scripts. unlike norns
+scripts, the code in a mod is loaded by matron when is starts up. mods can
+modify or extend the lua environment globally such that the changes are visible
+to all scripts.
+
+## how
+
+mods work by registering functions to be called by matron hooks. matron defines
+four hooks:
+
+* "system_post_startup" - called after matron has fully started and system state
+  has been restored but before any script is run
+* "system_pre_shutdown" - called when `SYSTEM > SLEEP` is selected from the menu
+* "script_pre_init" - called after a script has been loaded but before its
+  engine has started, pmap settings restored, and `init()` function called
+* "script_post_cleanup" - called after a script's `cleanup()` function has been
+  called, this normally occurs when switching between scripts
+
+multiple mods can each register their own callback function for a given hook.
+matron will call each registered function but the order in which the functions
+are called is arbitrary. any errors generated within a registered function will
+be caught and ignored.
+
+mods may additionally provide their own menu page accessible via the `SYSTEM >
+MODS` menu.
+
+## layout
+
+a mod is any norns script/project which contains a file called `lib/mod.lua`
+which allows a mod to be either a standalone project or simply functionality
+bundled along side an existing norns script.
+
+## using
+
+mods by their nature can have a negative impact on system stability or make
+system level changes which may not be universally welcome. by default a mod;
+even if installed, will not be loaded by matron. one must explicitly enable a
+mod and restart matron before its functionality will be available.
+
+to enable a mod go to `SYSTEM > MODS`. mods which are loaded will have a small
+dot to the left of their name in the listing. use `E2` to selected an item in
+the list and then use `E3` to enable or disable as appropriate. _unloaded_ mods
+will show a `+` to the right their name to indicate that they will be enabled
+(and loaded) on restart. _loaded_ mods will show a `-` to the right of their
+name indicating they will be disabled on restart.
+
+loaded mods which have a menu will have ` >` at the end of their name, pressing
+`K3` will enter the mod's menu.
+
+## developing
+
+see the `lib/mod.lua` file in this repo for more details.
+
+
---
+++ lib/mod.lua
@@ -0,0 +1,107 @@
+--
+-- require the `mods` module to gain access to hooks, menu, and other utility
+-- functions.
+--
+
+local mod = require 'core/mods'
+
+--
+-- [optional] a mod is like any normal lua module. local variables can be used
+-- to hold any state which needs to be accessible across hooks, the menu, and
+-- any api provided by the mod itself.
+--
+-- here a single table is used to hold some x/y values
+--
+
+local state = {
+  x = 0,
+  y = 0,
+}
+
+
+--
+-- [optional] hooks are essentially callbacks which can be used by multiple mods
+-- at the same time. each function registered with a hook must also include a
+-- name. registering a new function with the name of an existing function will
+-- replace the existing function. using descriptive names (which include the
+-- name of the mod itself) can help debugging because the name of a callback
+-- function will be printed out by matron (making it visible in mainden) before
+-- the callback function is called.
+--
+-- here we have dummy functionality to help confirm things are getting called
+-- and test out access to mod level state via mod supplied fuctions.
+--
+
+mod.hook.register("system_post_startup", "my startup hacks", function()
+  state.system_post_startup = true
+end)
+
+mod.hook.register("script_pre_init", "my init hacks", function()
+  -- tweak global environment here ahead of the script `init()` function being called
+end)
+
+
+--
+-- [optional] menu: extending the menu system is done by creating a table with
+-- all the required menu functions defined.
+--
+
+local m = {}
+
+m.key = function(n, z)
+  if n == 2 and z == 1 then
+    -- return to the mod selection menu
+    mod.menu.exit()
+  end
+end
+
+m.enc = function(n, d)
+  if n == 2 then state.x = state.x + d
+  elseif n == 3 then state.y = state.y + d end
+  -- tell the menu system to redraw, which in turn calls the mod's menu redraw
+  -- function
+  mod.menu.redraw()
+end
+
+m.redraw = function()
+  screen.clear()
+  screen.move(64,40)
+  screen.text_center(state.x .. "/" .. state.y)
+  screen.update()
+end
+
+m.init = function() end -- on menu entry, ie, if you wanted to start timers
+m.deinit = function() end -- on menu exit
+
+-- register the mod menu
+--
+-- NOTE: `mod.this_name` is a convienence variable which will be set to the name
+-- of the mod with it is being loaded. in order for the menu to work it must be
+-- registered with a name which matches the name of the mod in the dust folder.
+--
+mod.menu.register(mod.this_name, m)
+
+
+--
+-- [optional] returning a value from the module allows the mod to provide
+-- library functionality to scripts via the normal lua `require` function.
+--
+-- NOTE: it is important for scripts to use `require` to load mod functionality
+-- instead of the norns specific `include` function. using `require` ensures
+-- that only one copy of the mod is loaded. if a script were to use `include`
+-- new copies of the menu, hook functions, and state would be loaded replacing
+-- the previous registered functions/menu each time a script was run.
+--
+-- here we provide a single function which allows a script to get the mod's
+-- state table. using this in a script would look like:
+--
+-- local mod = require 'name_of_mod/lib/mod'
+-- local the_state = mod.get_state()
+--
+local api = {}
+
+api.get_state = function()
+  return state
+end
+
+return api
